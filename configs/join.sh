10d282773b30e253cc9cf90ea07991bec1d53a4c
orc1272 <orc1272@gmail.com>
2024-11-12 15:16:26 -0300

    atualizações

--- configs/join.sh
+++ configs/join.sh
@@ -1,1 +1,1 @@
-kubeadm join 192.168.56.10:6443 --token qsiq6k.gpufz7mkiqehcd2h --discovery-token-ca-cert-hash sha256:89b2228a0b5d8988e583458c7d8ba20ab4a5bf68d844aadc7901c558f4b1c0aa 
+kubeadm join 192.168.56.10:6443 --token k7dxyb.qidmhzeib7h5r4ew --discovery-token-ca-cert-hash sha256:8c8623c81956ad65a2abdc801176bd65742d3f6be3ff9d5b8063b5b1d5db1cb3 
